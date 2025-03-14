e3ec0996da8f9a7d239b70b47f20ec4d569ba851
arturShevts <arturshevtsov97@gmail.com>
2025-03-14 16:51:57 -0400

    feat(reminder) extract condition

--- src/app/components/reminder-form/reminder-form.component.ts
+++ src/app/components/reminder-form/reminder-form.component.ts
@@ -18,15 +18,27 @@ export class ReminderFormComponent implements OnInit {
     console.log(this.data)
     let reminder = this.data.reminder;
     let date = this.data.date;
+
+    if(reminder) {
       this.reminderFormGroup = new FormGroup({
-        text: new FormControl(reminder?.text||"", Validators.required), // <- bad approach for test covarage reports, but it's
-        year: new FormControl(reminder?.dateTime.getFullYear()|| date.getFullYear(), Validators.required),
-        month: new FormControl(reminder?.dateTime.getMonth()||date.getMonth(), Validators.required),
-        time: new FormControl(reminder?.dateTime||date.getTime()),  // Not required for all day events
-        city: new FormControl(reminder?.city||'', ),
-        color: new FormControl(reminder?.color||'', Validators.required),
+        text: new FormControl(reminder.text, Validators.required),
+        year: new FormControl(reminder.dateTime.getFullYear() , Validators.required),
+        month: new FormControl(reminder.dateTime.getMonth(), Validators.required),
+        time: new FormControl(reminder.dateTime),  // Not required for all day events
+        city: new FormControl(reminder.city ),
+        color: new FormControl(reminder.color, Validators.required),
+      })
+    } else {
+      this.reminderFormGroup = new FormGroup({
+        text: new FormControl( "", Validators.required),
+        year: new FormControl(  date.getFullYear(), Validators.required),
+        month: new FormControl( date.getMonth(), Validators.required),
+        time: new FormControl( date.getTime()),  // Not required for all day events
+        city: new FormControl( '', ),
+        color: new FormControl( '', Validators.required),
       })
 
+    }
    }
 
 
