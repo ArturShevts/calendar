035474da9bc57c8c4259f84e9b2e1e4b852c80c3
arturShevts <arturshevtsov97@gmail.com>
2025-03-14 15:35:49 -0400

    feat(calendar) add poc

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import {BehaviorSubject, distinctUntilChanged, map, Subject} from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { Reminder } from '../../interfaces/reminder';
 import { CalendarService } from '../../services/calendar.service';
@@ -8,13 +8,32 @@ import { MatDialog } from '@angular/material/dialog';
 import { ReminderFormComponent } from '../reminder-form/reminder-form.component';
 
 
+export interface Day  {
+  date: Date;
+  reminders: Reminder[];
+  weather: any;
+  display: boolean;
+  selected: boolean;
+  current: boolean;
+}
+
+
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit, OnDestroy {
+  selectedDate = new  BehaviorSubject<Date>(new Date());
+  $selectedDate = this.selectedDate.asObservable();
+  $selectedMonth = this.$selectedDate.pipe(
+    map((date) => date.getMonth(),
+      distinctUntilChanged())
+  );
 
+
+  tempDays: Day[] = [];
   onDestroy$ = new Subject<boolean>();
 
   constructor(
@@ -23,7 +42,16 @@ export class CalendarComponent implements OnInit, OnDestroy {
     private matDialog: MatDialog,
   ) { }
 
+
   ngOnInit(): void {
+    this.$selectedDate.subscribe((date) => {
+      console.log(date, "selected Date");
+    })
+    this.$selectedMonth.subscribe((month) => {
+      console.log(month, "selected Month");
+    })
+this.fillCalendar(new Date());
+
     this.calendarService.list(new Date())
       .pipe(takeUntil(this.onDestroy$))
       .subscribe((reminders: Reminder[]) => {
@@ -33,7 +61,6 @@ export class CalendarComponent implements OnInit, OnDestroy {
             weather: this.getWeather(reminder?.city ?? ''),
           };
         });
-        console.log(reminders);
       });
   }
 
@@ -42,6 +69,8 @@ export class CalendarComponent implements OnInit, OnDestroy {
     console.log(x);
     return x;
   }
+
+
 
   ngOnDestroy() {
     this.onDestroy$.next(true);
@@ -55,5 +84,55 @@ export class CalendarComponent implements OnInit, OnDestroy {
       },
     });
   }
+
+
+
+
+
+// CALENDAR COMPONENT
+
+public selectDate(selectedDate: Date) {
+   this.selectedDate.next(selectedDate);
+
+}
+
+
+public fillCalendar(selectedDate:Date) {
+    let firstDayMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1);
+
+    let firstDisplayDate =    new Date(selectedDate.getFullYear(), selectedDate.getMonth(), firstDayMonth.getDate() - firstDayMonth.getDay());
+
+    let firstSatOfNextMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 1);
+ let days:Day[] = [];
+for (let i = 0; i < 42; i++) {
+
+   let newDate = new Date(firstDisplayDate.getFullYear(), firstDisplayDate.getMonth(), firstDisplayDate.getDate() + i)
+  let newDay: Day = {
+    date: newDate,
+    reminders: [],
+    weather: {},
+    display: newDate.getMonth() === selectedDate.getMonth(),
+    selected : false,
+    current: newDate.toDateString() === new Date().toDateString()
+ }
+
+  days.push(newDay)
+}
+  console.log(days)
+this.tempDays = days
+}
+
+// year and month select
+
+public changeMonth(changeBy: number) {
+  this.selectedDate.next(new Date(this.selectedDate.value.getFullYear(), this.selectedDate.value.getMonth() + changeBy, 1));
+  this.fillCalendar(this.selectedDate.value);
+}
+
+
+public changeYear(changeBy:number) {
+  this.selectedDate.next(new Date(this.selectedDate.value.getFullYear() + changeBy, this.selectedDate.value.getMonth(), 1));
+  this.fillCalendar(this.selectedDate.value);
+}
 
 }
